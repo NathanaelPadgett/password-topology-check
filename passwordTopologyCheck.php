dd0f8f41304a62a180621ff8784364429d574be6
Nathanael Padgett <info@417solutions.com>
2018-10-22 15:00:18 -0500

    changed function name and now returning true/false as values

--- passwordTopologyCheck.php
+++ passwordTopologyCheck.php
@@ -4,7 +4,7 @@
 // @notes		doesn't work when # is passed in somewhere inside the $password
 // @attr		forbidden topology list from: http://blog.korelogic.com/blog/2014/04/04/pathwell_topologies
 // ------------------------------------------------------------------------------
-function passwordTopologyCheck($password){
+function common_password_topology($password){
     $forbidden = array("ullllldd","ulllllldd","ullldddd","llllllld","ullllllldd","ulllllld","ullllldddd","ulllldddd","lllllldd","ullllllld","ullllddd","ulldddds","llllllll","ulllllddd","llllllldd","llsddlddl","lllllllld","ullllldds","ulllllldddd","ulllllllldd","ulllllds","ulllllllld","ullllldddds","lllllllll","lllllllldd","ullllllddd","lllllddd","ullldddds","ullllllldddd","ulllllsdd","uuuuuudl","lllldddd","ddulllllll","ullsdddd","ulllldds","ullllllds","ddullllll","llllsddd","llllllllld","llllldddd","llllllllll","llllllddd","ullllllllldd","ullllllllld","ddddddul","ulllllllddd","ulllllldds","uuuuuuds","uudllldddu","ullllsdd","ulllllsd","lllsdddd","lllllldddd","ullllllldds","ddulllll","ulllllllds","ullllddds","ulllldddds","ulllsdddd","ullllsddd","ulllllldddds","ulllddds","llllsdddd","llllllsdd","lllllldds","ddddulll","dddddddd","ullllllsd","uldddddd","llllllsd","udllllllld","lllllllllll","lllllllllld","llllldds","llllddds","ulllllllldddd","uuuuuuuu","ulllsddd","ullllllsdd","ulllllddds","lllllsdd","ullllsdddd","ulllddddd","ulldddddd","ullddddd","llllllllldd","llllllldds","lllllllddd","llllllds","llldddds","uuullldddd","ulllllsddd","ulllllllsd","llllllllsd","llllllldddd","ulllllsdddd","lllllllds","lllldddds","ddddullll","uudllldddd");
     $pattern = "";
     for ($i=0; $i<strlen($password); $i++) {
@@ -14,9 +14,9 @@ function passwordTopologyCheck($password){
         if(preg_match("/[^\da-zA-Z]/",$password[$i])){$pattern .= "s";}
     }
     if(in_array($pattern, $forbidden)){
-    	return "Fail";
+    	return true;
     }else{
-    	return "Good";
+    	return false;
     }
 }
 ?>
